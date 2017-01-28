043ad73bfe599e0602ad4bd3a100f10ed267233e
Jonathan Hautzinger <jmhautzinger@gmail.com>
2017-01-28 04:09:33 -0600

    Initial Commit

---
+++ Assets/MainMenuSettings.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MainMenuSettings : MonoBehaviour {
+
+    public Text OverrideText;
+    public Text Walkperiod;
+    public Text RushPeriod;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
+}
