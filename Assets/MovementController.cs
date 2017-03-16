4ffd68e5c81ff9f846800c741cdab303d6f39a78
Jonathan Hautzinger <JonHautz9@users.noreply.github.com>
2017-03-16 17:37:46 -0500

    Cole's Initial TCP_Client

--- Assets/MovementController.cs
+++ Assets/MovementController.cs
@@ -43,5 +43,46 @@ public class MovementController : MonoBehaviour {
         void getXYZ ()
         {
             //here is where the TCP Client will run to communicate with the Control Application
+            Int32 port = 50040;		//command interface port
+            TcpClient client = new TcpClient ("127.0.0.1", port);
+            StreamWriter commandWrite = new StreamWriter(client.GetStream ());
+            StreamReader commandRead = new StreamReader (client.GetStream ());
+            //Boolean[] activeSensors = new Boolean[16];		//check to see which sensors are active
+            Byte[] data = new Byte[192];
+            string command;
+            /*for (int i ; i < 16 ; i++) {
+                command = string.Format ("SENSOR %d PAIRED?\r\n", i); //check which sensors are on and paired
+                data = System.Text.Encoding.ASCII.GetBytes(command);
+                StreamReader reader = new StreamReader (client.GetStream ());
+                stream.WriteAsync (data, 0, 100);
+                stream.ReadAsync (data, 0, 192);
+                command = System.Text.Encoding.ASCII.GetString (data);
+                if (command == "Yes") {
+                    activeSensors [i] = true;
+                    command = string.Format ("SENSOR %d SETMODE 2\r\n", i);
+                } else {
+                    activeSensors [i] = false;
+                }
+            }*/
+
+            port = 50042;
+            TcpClient accelerometerPort = new TcpClient (hostname, port);
+            NetworkStream accStream = accelerometerPort.GetStream ();
+            bool running = true;
+            float[] values = new float[48];
+            commandWrite.WriteLineAsync("START\r\n");
+            command = commandRead.ReadLineAsync ();
+            if (command == "OK") {
+                float leftX, leftY, leftZ, rightX, rightY, rightZ;
+                while (running) {
+                    accStream.ReadAsync (data, 0, 192);
+                    leftX = System.BitConverter.ToSingle (data, 0);
+                    leftY = System.BitConverter.ToSingle (data, 4);
+                    leftZ = System.BitConverter.ToSingle (data, 8);
+                    rightX = System.BitConverter.ToSingle (data, 12);
+                    rightY = System.BitConverter.ToSingle (data, 16);
+                    rightZ = System.BitConverter.ToSingle (data, 20);
+                }
+            }
         }
 }
