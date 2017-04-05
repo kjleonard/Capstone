04eea152d1b1c7d4e633d2f1e993a2bb02b02d98
AndrewSusman <AndrewSusman@gmail.com>
2017-04-05 06:27:57 -0500

    -Began adding scripting to EndScreen

---
+++ Assets/EndScreen.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndScreen : MonoBehaviour {
+
+    Text lblAvoidedCount;
+    Text lblDistance;
+
+	// Use this for initialization
+	void Start () {
+
+        int obstaclesHit = PlayerPrefs.GetInt("selObstaclesHit");
+        int obstacleTotal = PlayerPrefs.GetInt("selObstacleFrequency");
+        int duration = PlayerPrefs.GetInt("selDuration");
+        float speed = PlayerPrefs.GetFloat("selSpeed");
+
+        lblAvoidedCount.text = (obstacleTotal - obstaclesHit).ToString() + " / " + obstacleTotal.ToString();
+        lblDistance.text = (duration * speed).ToString() + " meters";
+
+        //GameObject lblAvoidedCount = GameObject.Find("lblAvoidedCount");
+        //lblAvoidedCount.GetComponent
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
