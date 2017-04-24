61dda8243fd6755d5a356c0db6d0e6190857a742
AndrewSusman <AndrewSusman@gmail.com>
2017-04-23 23:36:03 -0500

    -Add StartMenu.cs

---
+++ Assets/StartMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR;
+
+public class StartMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        // Set initial values since Player Prefs are only written to if the dropdown value is changed
+
+        Cursor.visible = true;
+        VRSettings.enabled = false;
+
+        PlayerPrefs.SetFloat("selSpeed", (float) 0.9);
+        PlayerPrefs.SetInt("selDuration", 0);
+        PlayerPrefs.SetInt("selObstacleType", 0);
+        PlayerPrefs.SetInt("selObstacleFrequency", 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
